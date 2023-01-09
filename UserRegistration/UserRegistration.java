e3e39e5b4426d521427e299fdfe5aa2862c9fba5
nikhil47nick <nikhiljoji94@gmail.com>
2023-01-09 17:49:28 +0530

    Added:UserRegistration UC2

--- UserRegistration/UserRegistration.java
+++ UserRegistration/UserRegistration.java
@@ -1,22 +1,35 @@
 package com.bridgelabz.Day19.UserRegistration;
 
 
+import java.util.Scanner;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
 
-    public static void main(String[] args) {
-
-        String firstName = "Nikhil";
-
-        boolean namePattern = Pattern.matches("^[A-Z]{1}[a-z]{2,}",firstName);
+    public static void firstNameCheck(String name){
+        boolean namePattern = Pattern.matches("^[A-Z]{1}[a-z]{2,}",name);
         if(namePattern){
             System.out.println("Matched");
         }else{
             System.out.println("Not Matched");
         }
+    }
 
+    public static void lastNameCheck(String name){
+        boolean namePattern = Pattern.matches("^[A-Z]{1}[a-z]{2,}",name);
+        if(namePattern){
+            System.out.println("Matched");
+        }else{
+            System.out.println("Not Matched");
+        }
+    }
 
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+        System.out.println("Please Enter FirstName");
+        firstNameCheck(scr.next());
+        System.out.println("Please Enter FirstName");
+        lastNameCheck(scr.next());
 
     }
 }
