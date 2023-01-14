9b2b216851acf2219a175fc7d0fb5933046144b4
Nikhil Joji <59815741+nikhil47nick@users.noreply.github.com>
2023-01-14 12:10:19 +0530

    Added: Email Pattern

---
+++ UserRegistration.java
@@ -0,0 +1,61 @@
+package com.bridgelabz.Day19.UserRegistration;
+
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class UserRegistration {
+
+    public static void patterResult(boolean pattern){
+        if(pattern){
+            System.out.println("Valid");
+        }else{
+            System.out.println("Invalid!");
+        }
+    }
+    public static void firstNameCheck(String name){
+        boolean namePattern = Pattern.matches("^[A-Z]{1}[a-z]{2,}",name);
+        patterResult(namePattern);
+    }
+
+    public static void lastNameCheck(String name){
+        boolean namePattern = Pattern.matches("^[A-Z]{1}[a-z]{2,}",name);
+        patterResult(namePattern);
+    }
+
+    public static void emailCheck(String name){
+        boolean emailPattern = Pattern.matches("^(?=.*[a-z0-9._-])[a-z0-9._-]*[@]{1}(?=.*[a-z0-9.])[a-z0-9.]+[a-z]{2,4}$",name);
+        patterResult(emailPattern);
+    }
+
+    public static void phoneNumberCheck(String name){
+        boolean numberPattern = Pattern.matches("^[0-9]{2} [0-9]{10}",name);
+        patterResult(numberPattern);
+    }
+
+    public static void passwordCheck(String name){
+        boolean emailPattern = Pattern.matches("(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^\\d\\w])[A-Za-z0-9#?!@$%^&*-]{8,}",name);
+        patterResult(emailPattern);
+    }
+
+    public static void main(String[] args) {
+        Scanner scr = new Scanner(System.in);
+//        passwordCheck(scr.next());
+//        System.out.println("Please Enter FirstName");
+//        firstNameCheck(scr.next());
+//        System.out.println("Please Enter LastName");
+//        lastNameCheck(scr.next());
+//        System.out.println("Please ENter email");
+//        emailCheck(scr.next());
+
+        emailCheck("abc@yahoo.com");
+        emailCheck("abc-100@yahoo.com");
+        emailCheck("abc.100@yahoo.com");
+        emailCheck("abc111@abc.com");
+        emailCheck("abc-100@abc.net");
+        emailCheck("abc.100@abc.com.au");
+        emailCheck("abc@1.com");
+        emailCheck("abc@gmail.com.com");
+        emailCheck("abc+100@gmail.com");
+    }
+}
